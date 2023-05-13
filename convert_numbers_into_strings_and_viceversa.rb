350e8e1658e8fea4b9aa9da81e1137a1926ef8ad
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-05-13 17:32:49 +0000

    coverting numbers into booleans and vice versa

---
+++ convert_numbers_into_strings_and_viceversa.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# converting string to number
+str = '5'
+p str
+p str.class
+
+p str.to_i.class
+
+p str.to_f
+p str.to_f.class
+
+# converting number to string
+
+number = 10
+p number
+p number.class
+p number.to_s.class
+
+p number.to_f
+p number.to_f.class
+
+# converting float to integer
+pi = 3.14
+p pi
+p pi.class
+p pi.to_i.class
