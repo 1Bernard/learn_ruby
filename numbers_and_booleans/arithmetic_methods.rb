847d216612437a8928541f331c0f1424d6b87899
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-06-03 09:58:29 +0000

    object methods with parameters

---
+++ numbers_and_booleans/arithmetic_methods.rb
@@ -0,0 +1,7 @@
+# arithmetic methods
+p 1 + 2 # addition operator
+p 1 - 3 # subtraction operator
+p 1 * 4 # multiplication operator
+p 1 / 5 # division operator
+p 12.0 / 5.0 # division on floats
+p 10 % 5 # modulus operator
