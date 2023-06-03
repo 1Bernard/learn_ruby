283bb5fcefeec5c201c1f1084281e5f615c6a5ee
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-06-03 09:17:01 +0000

    numbers_and_booleans\less_than_and_greater_than_operation

---
+++ numbers_and_booleans/less_than_and_greater_than_operation.rb
@@ -0,0 +1,16 @@
+# less than operator
+p 1 < 8
+p 1 < 5
+
+# greater than operator
+p 1 > 8
+p 1 > 5
+
+# less than or equal to operator
+p 1 <= 8
+p 1 <= 5
+
+# greater than or equal to operator
+p 1 >= 8
+p -1 >= -5
+
