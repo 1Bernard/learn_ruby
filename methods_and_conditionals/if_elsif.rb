09835ced9be2199c0d7476019693044be48401b5
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-06-24 16:57:58 +0000

    if and elsif

---
+++ methods_and_conditionals/if_elsif.rb
@@ -0,0 +1,19 @@
+color = "Green"
+
+if color == "Red"
+  puts "red is rad"
+elsif color == "Green"
+  puts "green is great"
+elsif color == "Yellow"
+  puts "yay for yellow!"
+end
+
+number = 55
+
+if number < 25
+  puts "That is is a low number"
+elsif number < 50
+  puts "That is a number in the middle"
+elsif number < 75
+  puts "That is a big number"
+end
