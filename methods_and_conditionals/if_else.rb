ba46a6fe12a61ed1cc111abdcef2720ada71bcab
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-06-25 02:27:19 +0000

    if and else

---
+++ methods_and_conditionals/if_else.rb
@@ -0,0 +1,19 @@
+grade = "F"
+
+if grade == "A"
+  puts "That's an excellent grade. Good job."
+elsif grade == "B"
+  puts "That's an good grade. Let's bring it up next time."
+else
+  puts "Unacceptable"
+end
+
+def odd_or_even(number)
+  if number.odd?
+    "That number is odd"
+  else
+    "That number is even"
+  end
+end
+
+p odd_or_even(4)
