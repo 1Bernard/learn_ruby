708f960ed67da5c5a596044cef7fbbbd22358a2b
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-09-13 21:18:35 +0000

    overwrite array elements

---
+++ arrays_I/acess_sequential_array_elements_with_bracket_syntax.rb
@@ -0,0 +1,4 @@
+number = [1, 3, 5, 7, 9, 15, 21]
+
+p numbers[0]
+p numbers[0, 1]
---
+++ arrays_I/overwrite_array_elements.rb
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Grape", "Orange", "Banana"]
+fruits[4] = "Raspberry"
+p fruits
+
+fruits[5] = "Strawberry"
+p fruits
+
+fruits[-1] = "Banana"
+p fruits
