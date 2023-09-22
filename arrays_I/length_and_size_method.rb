e8d300f8fbaaeff92541076b9ce1012583842907
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-09-22 03:09:47 +0000

    first and last methods

---
+++ arrays_I/length_and_size_method.rb
@@ -0,0 +1,4 @@
+# the lenght or size method when called on an array counts the number of elements in the array. It counts objects in the array including nil values.
+
+p [1, nil, 2, 3, nil, 4, 5].length
+p [1, nil, 2, 3, nil, 4, 5].size
