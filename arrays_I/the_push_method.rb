e75067d6ecc5f77147d878e7f2d62bd20adf891d
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2023-10-08 06:33:07 +0000

    the push method

---
+++ arrays_I/the_push_method.rb
@@ -0,0 +1,8 @@
+locations = ["House", "Airpuort", "Bar"]
+
+locations.push("Restaurant", "Hotel")
+p locations
+
+# the shovel operator is the shortcut to the push method
+locations << "Restaurant" << "Hotel"
+p locations
